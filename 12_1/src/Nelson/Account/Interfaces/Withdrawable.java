8a30b16b180b3919340eb59a3df010a031cdf7d7
Suliman Sharif <sharifsuliman1@gmail.com>
2016-02-16 23:17:36 -0600

    revert head to 12-1 first edit

--- 12_1/src/Nelson/Account/Interfaces/Withdrawable.java
+++ 12_1/src/Nelson/Account/Interfaces/Withdrawable.java
@@ -1,10 +1,5 @@
-package cannata;
+package Nelson.Account.Interfaces;
 
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
+public interface Withdrawable{
+    void withdraw(double amount);
 }
--- 12_1/src/Nelson/ui/AccountApp.java
+++ 12_1/src/Nelson/ui/AccountApp.java
@@ -1,10 +1,50 @@
-package cannata;
+package Nelson.ui;
+import java.util.Scanner;
 
-public class HelloWorld {
+public class AccountApp{
+    public static void main(String args[]){
+        //initiate scanner for user input
+        Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Welcome to the Account Calculator\n");
+        System.out.println("Starting Balance:");
+        System.out.println("Checking:");
+        System.out.println("Enter the transactions for the month\n");
+
+        String choice = "y";
+        //while loop while user chooses y to continue
+        while(choice.equalsIgnoreCase("y")){
+            String inputAmount;
+            double numAmount;
+            //ask for user input and store
+            System.out.println("Withdrawal or deposit? (w/d):");
+            String input = sc.nextLine();
+
+            //withdraw
+            if(input.equalsIgnoreCase("w")){
+                System.out.println("Amount:");
+                inputAmount = sc.nextLine();
+                numAmount = Double.parseDouble(inputAmount);
+                System.out.println("Continue? (y/n):");
+                choice = sc.nextLine();
+
+            //deposit
+            }else if(input.equalsIgnoreCase("d")){
+                System.out.println("Amount:");
+                inputAmount = sc.nextLine();
+                numAmount = Double.parseDouble(inputAmount);
+                if(numAmount>=10000){
+                    System.out.println("Cannot deposit more than $10,000");
+                }
+                System.out.println("Continue? (y/n):");
+                choice = sc.nextLine();
+
+            //return error for invalid choice
+            }else{
+                System.out.println("Invalid entry.\nContinue? (y/n):");
+                choice = sc.nextLine();
+            }
+        }
+        System.out.print("Bye!");
     }
-
 }
